73624891b2c83c57554c78178d270ee362266185
David Vink <david@tinqwiseimmersive.nl>
2018-04-19 19:13:16 +0200

    Initial

---
+++ Project Plane p/Assets/Scripts/CustomTimer.cs
@@ -0,0 +1,60 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class CustomTimer : MonoBehaviour {
+	public static CustomTimer instance;
+
+	public Animation anim;
+
+	public int minuteLeap;
+	public Vector2 startingTime;
+	public Text timer;
+
+	public Vector2 currentTime;
+
+	private void Awake() {
+		instance = this;
+		timer.text = IntToTimeString(startingTime);
+		currentTime = startingTime;
+	}
+	private string IntToTimeString(Vector2 time){
+		Debug.Log(time);
+		string newTimeString = "";
+
+		if(time.x < 10){
+			newTimeString += "0";
+		}
+		newTimeString += time.x + ":";
+		if(time.y < 10){
+			newTimeString += "0";
+		}
+		newTimeString += time.y;
+
+		return newTimeString;
+
+	}
+	public void Update(){
+		if(anim["Main"].speed == 1){
+			ProgressTime();
+		}
+	}
+	public void ProgressTime(){
+		Debug.Log("Progressing Time");
+		float time = anim["Main"].time;
+		Debug.Log("Base Time Value: " + time);
+		Debug.Log("Rounded Time Value: " + (int)time);
+		Debug.Log("Weird Number: " + (int)((((int)time * 10) / 60) * 60));
+		currentTime.y = (int)time * 10 -(int)((((int)time * 10) / 60) * 60);
+		currentTime.x = (int)anim["Main"].time / 6 + startingTime.x; 
+		Debug.Log("Pre check: " + currentTime);
+		Debug.Log("After check: " + currentTime);
+
+		timer.text = IntToTimeString(currentTime);
+	}
+	public void ResetTimer(){
+		timer.text = IntToTimeString(startingTime);
+		currentTime = startingTime;
+	}
+}
---
+++ Project Plane p/Assets/Scripts/Timeline.cs
@@ -0,0 +1,57 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Timeline : MonoBehaviour {
+	public Slider slider;
+	public Animation anim;
+
+	bool playing;
+
+	public Sprite pause;
+	public Sprite play;
+	public Image button;
+
+	public void Start(){
+		anim.Play("Main");
+		anim["Main"].speed = 0;
+	}
+	public void Update(){
+		if(anim["Main"].speed == 1){
+			slider.value = anim["Main"].normalizedTime;
+		}
+		else{
+			anim["Main"].normalizedTime = slider.value;
+		}
+		if(!anim.isPlaying){
+			anim.Play("Main");
+			button.sprite = play;
+			anim["Main"].speed = 0;
+			playing = false;
+			CustomTimer.instance.ResetTimer();
+		}
+	}
+	public void Play(){
+		if(playing){
+			anim["Main"].speed = 0;
+			playing = false;
+			button.sprite = play;
+			CustomTimer.instance.ProgressTime();
+		}
+		else{
+			anim["Main"].speed = 1;
+			playing = true;
+			button.sprite = pause;
+			CustomTimer.instance.ProgressTime();			
+		}
+	}
+	public void Pauze(){
+		if(playing){
+			anim["Main"].speed = 0;
+			playing = false;
+			button.sprite = play;
+			CustomTimer.instance.ProgressTime();			
+		}	
+	}
+}
